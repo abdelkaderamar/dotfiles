7c4cfdb5b8aae35205fcd598238bce1a216aba25
Abdelkader Amar <abdelkader.amar@gmail.com>
2018-04-01 23:53:58 +0200

    Addind a sheel script to send SMS (via Free service)

---
+++ scripts/sms.sh
@@ -0,0 +1,38 @@
+#! /bin/bash -u
+
+function usage()
+{
+  e_header "Usage sms.sh <text>"
+  echo
+  e_arrow "$@"
+}
+
+. $HOME/.bash_logging
+
+if [ -z ${FREE_USER+x} ]; then usage "Please set FREE_USER variable" ; exit 1; fi
+if [ -z ${FREE_PASS+x} ]; then usage "Please set FREE_PASS variable"; exit 1; fi
+
+if [ $# -lt 1 ]
+then
+  usage "Please set FREE_USER and FREE_PASS variables"
+  exit 1
+fi
+msg=`echo "$1" | sed -e 's/\n/%0A/g'`
+echo $msg
+send=$(curl -i --insecure "https://smsapi.free-mobile.fr/sendmsg?user=$FREE_USER&pass=$FREE_PASS&msg=$msg" 2>&1)
+receive=$(echo "$send" | awk '/HTTP/ {print $2}')
+case $receive  in
+  200)
+    e_success "Message sent correctly"
+    ;;
+  400)
+    e_error "Credentials are incorrect. Please check FREE_PASS and FREE_USER variable" ;;
+  402)
+    e_error "Too many SMS sent in a short period" ;;
+  403)
+    e_error "Service not activated" ;;
+  500)
+    e_error "Error in server side. Please retry later" ;;
+  *) e_error "Error code = [$receive]";;
+esac
+exit 0
