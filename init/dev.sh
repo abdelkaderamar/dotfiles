00de367a435ee3de2ad030ceaa2577ebaeed1ca0
Abdelkader Amar <abdelkader.amar@gmail.com>
2018-04-08 14:15:01 +0200

    Adding gtest library to dev profile (compilation from source)

--- init/dev.sh
+++ init/dev.sh
@@ -12,8 +12,8 @@ esac
 
 if [ ! -z "$go_suffix" ]
 then
-  if [ -d $SOFT_DIR/go-$GO_VERSION ]
   e_header "Installing Go language go-$GO_VERSION"
+  if [ -d $SOFT_DIR/go-$GO_VERSION ]
   then
     e_arrow "An existing Go directory exists [$SOFT_DIR/go-$GO_VERSION]"
   else
@@ -30,3 +30,29 @@ then
 else
   e_warn "Unknwon architecture [$go_suffix]=> GO will not be installed"
 fi
+# End Go installation
+
+
+# Google-test installation
+GTEST_VERSION=1.8.0
+e_header "Installing google-test library $GTEST_VERSION"
+if [ -f /usr/local/include/gtest/gtest.h -a -f /usr/local/lib/libgtest.a ]
+then
+  e_arrow "Gtest is already installed"
+else
+  gtest_filename=release-${GTEST_VERSION}.tar.gz
+  gtest_url=https://github.com/google/googletest/archive/${gtest_filename}
+  tmp_dir=$(mktemp -d)
+  cd $tmp_dir && e_arrow downloading ... && \
+    wget -q "$gtest_url" && tar xf $gtest_filename  && \
+    mkdir build && cd build && \
+    cmake ../googletest-release-${GTEST_VERSION} > /dev/null && \
+    e_arrow building ... && \
+    make -j 2 > /dev/null && \
+    e_arrow installing ... && \
+    sudo make install > /dev/null && \
+    rm -fr "${tmp_dir}"
+fi
+e_arrow "Gtest installaton done with error code $?"
+
+# End Google-test installation
