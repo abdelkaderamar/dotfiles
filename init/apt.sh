27c2eef6e5f50ff517c4ebc2dda7f8af9d101736
Abdelkader Amar <abdelkader.amar@gmail.com>
2018-09-06 11:23:19 +0200

    Adding pandoc

--- init/apt.sh
+++ init/apt.sh
@@ -19,7 +19,7 @@ apt_packages+=(npm)
 apt_packages+=(docker.io)
 
 # maybe 
-apt_packages+=(apt-file hugo)
+apt_packages+=(apt-file hugoi pandoc)
 
 # Ubuntu distro release name, eg. "xenial"
 release_name=$(lsb_release -c | awk '{print $2}')
