fe4e6c5a4084edd19b4e248ce28c40b9b42f9770
Abdelkader Amar <abdelkader.amar@gmail.com>
2019-03-26 08:58:33 +0100

    Adding some apt packages

--- init/apt.sh
+++ init/apt.sh
@@ -19,7 +19,7 @@ apt_packages+=(npm)
 apt_packages+=(docker.io)
 
 # maybe
-apt_packages+=(apt-file pandoc soundconverter mp3info vlc)
+apt_packages+=(apt-file pandoc soundconverter mp3info vlc mkvtoolnix gparted keepass2)
 
 # Ubuntu distro release name, eg. "xenial"
 release_name=$(lsb_release -c | awk '{print $2}')
