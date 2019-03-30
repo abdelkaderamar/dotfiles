3691954faac31807c56a891d1f82532fa291ee4b
Abdelkader Amar <abdelkader.amar@gmail.com>
2019-03-30 10:03:44 +0100

    Updating exec_then_sms script

--- scripts/exec_then_sms.sh
+++ scripts/exec_then_sms.sh
@@ -7,6 +7,14 @@ usage()
   echo "Usage : cmd [{arguments}]"
 }
 
+add_log_separator() {
+  LOG_FILE="$1"
+  echo >> "$LOG_FILE"
+  echo "##################################################" >> "$LOG_FILE"
+  echo >> "$LOG_FILE"
+
+}
+
 if [ $# -eq 0 ]
 then
   usage
@@ -19,13 +27,26 @@ then
   exit 0
 fi
 
-CMD="$1"
+date +'%Y-%m-%d-%H:%M:%S'
 
+CMD="$1"
 shift
 
-$CMD "$@"
+date=$(date +'%Y-%m-%d-%H:%M:%S')
+LOG=`basename $0`-$CMD-$date.log
+ERR=${LOG%.log}.err
 
+echo Executing the following command at $(date) >> "$LOG"
+echo "   $CMD $@" >> "$LOG"
+
+add_log_separator "$LOG"
+
+$CMD "$@" >> "$LOG" 2>> "$ERR"
 res=$?
+
+add_log_separator "$LOG"
+
+echo "Finishing the execution at $(date) with result $res" >> $LOG
 
 if [ $res -eq 0 ]
 then
@@ -37,6 +58,15 @@ else
 fi
 HOSTNAME=$(hostname)
 
-MSG_TO_SEND="$STATUS. Command {$CMD} $verb at $(date) on host {$HOSTNAME}"
+MSG_TO_SEND="$STATUS. Command {$CMD} $verb at $(date) on host {$HOSTNAME}. Arguments=$@"
 
 sms.sh "$MSG_TO_SEND"
+
+if [ $? -eq 0 ]
+then
+  echo "SMS sent successfully" >> $LOG
+else
+  echo "Failed to send notification SMS [$res]" >> "$LOG"
+fi
+
+exit "$res"
