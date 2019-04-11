6bcb54f8f380c593a05cf964396bcab95806cb4a
Abdelkader Amar <abdelkader.amar@gmail.com>
2019-04-11 07:52:05 +0200

    Updating check_rclone script

--- scripts/check_rclone.sh
+++ scripts/check_rclone.sh
@@ -7,19 +7,24 @@ do
     pidof rclone
     res=$?
 
-    if [ $res -ne 0 ]
+    if [ $res -eq 0 ]
     then
-	sms.sh "Rclone is not running on $(hostname)"
 	continue
     fi
-
-    rclone lsd hubic:default
-    res=$?
-
+    
     if [ $res -ne 0 ]
     then
-	sms.sh "Rclone is not connected on $(hostname)"
+	MSG="Rclone is not running on $(hostname)."
+
+	# check if we can connect to hubic
+	rclone lsd hubic:default
+	res=$?
+
+	if [ $res -ne 0 ]
+	then
+	    MSG+=" Rclone is not connected on $(hostname)"
+	fi
+	sms.sh "$MSG"
 	continue
     fi
-
 done
