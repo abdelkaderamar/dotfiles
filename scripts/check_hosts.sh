0bced151ff88776e9e0785ec8195653f3f421c51
Abdelkader Amar <abdelkader.amar@gmail.com>
2019-04-13 17:58:24 +0200

    Updating check_host script

--- scripts/check_hosts.sh
+++ scripts/check_hosts.sh
@@ -12,20 +12,46 @@ then
     exit 1
 fi
 
+start_hour=9
+end_hour=21
+
+hosts=()
+while [ $# -gt 0 ]
+do
+  case "$1" in
+      '-start')  shift
+		 start_hour=$1
+		 ;;
+      '-end')  shift
+	       end_hour=$1
+	       ;;
+      -*)  echo "Unknwon option $1"
+	     ;;
+      *)  hosts+=($1)
+	;;
+  esac
+  shift
+done
+
+
 while true
 do
+    current_hour=$(date +'%H')
+
+    if [ $current_hour -ge $start_hour -a $current_hour -lt $end_hour ]
+    then
+	for host in  "${hosts[@]}"
+	do
+	    ssh "$host" "ls > /dev/null"
+	    res=$?
+	    
+	    if [ $res -ne 0 ]
+	    then
+		sms.sh "The host $host is unreachable"
+	    fi
+	done
+    fi
+    
     sleep 1800
-
-    for i in "$@"
-    do
-	host=$i
-	ssh "$host" "ls > /dev/null"
-	res=$?
-
-	if [ $res -ne 0 ]
-	then
-	    sms.sh "The host $host is unreachable"
-	fi
-    done
     
 done
