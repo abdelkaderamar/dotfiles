336d36a92fcd5266ce32ab0071645fdf2ba9de5b
Abdelkader Amar <abdelkader.amar@gmail.com>
2019-09-28 09:18:29 +0200

    Updating server64 config

--- init/apt_server64.sh
+++ init/apt_server64.sh
@@ -4,8 +4,11 @@ apt_packages+=( wpasupplicant )
 apt_packages+=( ifupdown )
 
 add_docker
+
 apt_packages+=( apache2 )
 apt_packages+=( mariadb-server )
 add_php
 
 apt_packages+=( imagemagick )
+
+install_node
