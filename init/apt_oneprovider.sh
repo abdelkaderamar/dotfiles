dfc80d312df057c6762f76e1bb7b13fd432e172a
Abdelkader Amar <abdelkader.amar@gmail.com>
2019-12-28 00:51:54 +0100

    Adding saleor requirements

--- init/apt_oneprovider.sh
+++ init/apt_oneprovider.sh
@@ -24,5 +24,7 @@ apt_packages+=( postgresql )
 
 # For saleor
 apt_packages+=( build-essential python3-dev python3-pip python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info )
+apt_packages+=( python3-venv )
+
 
 
