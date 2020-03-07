7aac7cb79fe7475c95bba9088db128c66def1817
Abdelkader Amar <abdelkader.amar@gmail.com>
2020-03-07 18:02:07 +0100

    Updating check_music_dir.sh script

--- scripts/check_music_dir.sh
+++ scripts/check_music_dir.sh
@@ -183,8 +183,7 @@ process_artist_dir() {
 	    fi
 	elif [ "$album_name" = "Singles" -o "$album_name" = "EPs" -o \
 			     "$album_name" = "$artist - Singles" -o "$album_name" = "$artist - EPs" -o \
-			     "$album_name" = "$artist - The Best Of"
-	     ]
+			     "$album_name" = "$artist - The Best Of"  ]
 	then
 	    if ( $process_files )
 	    then
