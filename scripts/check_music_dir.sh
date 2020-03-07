5fffbfff7747f8d48fe867ff147a068f65095e3b
Abdelkader Amar <abdelkader.amar@gmail.com>
2020-03-08 00:35:10 +0100

    Updating check_music_dir.sh script

--- scripts/check_music_dir.sh
+++ scripts/check_music_dir.sh
@@ -176,7 +176,13 @@ process_artist_dir() {
 	regexp="$artist - ([12][0-9][0-9][0-9]) - (.*)"
 	regexp2="$artist - ([12][0-9]{3}-[12][0-9]{3}) - (.*)"
 	#echo "$regexp vs $album_name"
-	if [ "$album_name" =~ "$regexp" -o "$album_name" =~ "$regexp2" ]
+	if {[ "$album_name" =~ $regexp ]]
+	then
+	    if ( $process_files )
+	    then
+		process_album "$d" "$artist"
+	    fi
+	elif [[ "$album_name" =~ $regexp ]]
 	then
 	    if ( $process_files )
 	    then
