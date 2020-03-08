32ff3086b3b4dfd07de6d5fdace440747a04d598
Abdelkader Amar <abdelkader.amar@gmail.com>
2020-03-08 13:01:57 +0100

    Adding clean_music_files.sh

---
+++ scripts/clean_music_files.sh
@@ -0,0 +1,25 @@
+#! /bin/bash -u
+
+usage() {
+    :
+}
+
+replace_special_character() {
+    char="$1"
+    new_char="$2"
+    dir="$3"
+
+    temp_file=$(mktemp)
+    find "$dir" -name "*${char}*" > "$temp_file"
+
+    while read f
+    do
+	new_filename=$(echo "$f" | sed "s/${char}/${new_char}/g")
+        mv "$f" "$new_filename"
+    done < "$temp_file"
+    rm -f "$temp_file"
+    
+}
+
+replace_special_character – - "$1"
+
