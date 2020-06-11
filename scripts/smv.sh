70ea982e1cebf4dfa61486d28204db2957732dc4
Abdelkader Amar <abdelkader.amar@gmail.com>
2020-06-11 14:16:36 +0200

    Updating smv.sh (Adding -l flag (transfer limit))

--- scripts/smv.sh
+++ scripts/smv.sh
@@ -1,4 +1,4 @@
-#!  /bin/bash -u
+#!  /bin/bash -ux
 #
 # Abdelkader Amar
 #
@@ -8,8 +8,12 @@ source $HOME/.bash_logging
 
 function usage()
 {
-  e_header Usage: $(basename "$0") '<HOST> [files]'
-  echo
+    e_header Usage: $(basename "$0") '<HOST> [-l <limit in kilobits>|-h|-q] [files]'
+    e_arrow    '-q        quiet mode (no log file)'
+    e_arrow    '-h HOST   remote hostname'
+    e_arrow    '-l LIMIT  set transfer limit in kilo bits (divide by 8 to equivalent in KB'
+    e_arrow    '-d DIR    set remote dir (default is $HOME/smv_files)'
+    echo
 }
 
 function append()
@@ -25,20 +29,24 @@ function append()
 HOST=''
 REMOTE_DIR=smv_files
 FILES=()
+LIMIT=0
 
 while [ $# -gt 0 ]
 do
   case "$1" in
-    -h) shift
-    HOST=$1
-    ;;
-    -d) shift
-    REMOTE_DIR=$1
-    ;;
-    -q) 
-    ;;
-    *) FILES+=("$1")
-    ;;
+      -h) shift
+	  HOST=$1
+	  ;;
+      -d) shift
+	  REMOTE_DIR="$1"
+	  ;;
+      -q) 
+	  ;;
+      '-l') shift
+	    LIMIT="$1"
+	    ;;
+      *) FILES+=("$1")
+	 ;;
   esac
   shift
 done
@@ -69,17 +77,22 @@ ssh "$HOST" "mkdir -p $REMOTE_DIR"
 
 for file in "${FILES[@]}"
 do
-  append "$LOG_FILE"  "Start copying [$file] ...."
-  scp -r -p "$file" "$HOST":"$REMOTE_DIR" >> "$LOG_FILE" 2>&1
-  res=$?
-  if [ $res -eq 0 ]
-  then
-    append "$LOG_FILE" "... copying $file succeed => delete it"
-    rm -fr "$file"  && append "$LOG_FILE"  "... $file deleted"
+    append "$LOG_FILE"  "Start copying [$file] ...."
+    if [ "$LIMIT" -eq 0 ]
+    then
+	scp -r -p "$file" "$HOST":"$REMOTE_DIR" >> "$LOG_FILE" 2>&1
+    else
+	scp -r -p -l "$LIMIT" "$file" "$HOST":"$REMOTE_DIR" >> "$LOG_FILE" 2>&1
+    fi
     res=$?
-  else
-    append "$LOG_FILE"  "failed to copy remotely $file (error code=[$res])" >> $LOG_FILE
-  fi
+    if [ $res -eq 0 ]
+    then
+	append "$LOG_FILE" "... copying $file succeed => delete it"
+	rm -fr "$file"  && append "$LOG_FILE"  "... $file deleted"
+	res=$?
+    else
+	append "$LOG_FILE"  "failed to copy remotely $file (error code=[$res])" >> $LOG_FILE
+    fi
 done
 
 append "$LOG_FILE" "Done at $(date)"
