4daf844f5c9ca55670d4dd4a4a093b155c5d0d81
Abdelkader Amar <abdelkader.amar@gmail.com>
2022-08-25 20:55:44 +0200

    Update nodejs version

--- init/apt_functions.sh
+++ init/apt_functions.sh
@@ -57,7 +57,7 @@ install_node()
 
     e_arrow "Installing node"
 
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
     sudo apt-get install -y nodejs
 
 }
