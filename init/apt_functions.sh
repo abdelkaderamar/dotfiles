18be0aa113213cd2e28ee7df6a8ff6fbdab18245
Abdelkader Amar <abdelkader.amar@gmail.com>
2022-08-25 19:07:31 +0000

    Use lts version for nodejs

--- init/apt_functions.sh
+++ init/apt_functions.sh
@@ -57,7 +57,6 @@ install_node()
 
     e_arrow "Installing node"
 
-    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+    curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
     sudo apt-get install -y nodejs
-
 }
