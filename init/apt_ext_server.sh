7ad8045d23c0a196309ba474479b1e1261f594bd
Abdelkader Amar <abdel@_HOSTNAME_>
2022-11-14 11:10:10 +0100

    Adding sqlite3

--- init/apt_ext_server.sh
+++ init/apt_ext_server.sh
@@ -31,5 +31,18 @@ apt_packages+=( ufw fail2ban )
 #apt_packages+=( build-essential python3-dev python3-pip python3-cffi libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info )
 #apt_packages+=( python3-venv )
 
+##############
+# Python 
+
+apt_packages+=( python3-virtualenv python3-pip )
+
+##############
+# Apache
+
+apt_packages+=( libapache2-mod-wsgi-py3 )
+
+
+apt_packages+=( sqlite3 )
+
 
 
