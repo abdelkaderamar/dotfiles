ff1d428596f26deb3be5b14532e1dd95a69acb68
Abdelkader Amar <abdelkader.amar@gmail.com>
2023-01-15 10:52:22 +0100

    [pi4] Add mp3splt and remove Kodi

--- init/apt_pi4.sh
+++ init/apt_pi4.sh
@@ -2,9 +2,10 @@ apt_packages+=( nfs-kernel-server )
 
 apt_packages+=( openjdk-11-jdk )
 
-apt_packages+=( kodi )
+apt_packages+=( mp3splt )
 
 apt_packages+=( apache2 )
 apt_packages+=( mariadb-server )
+
 add_php
 
